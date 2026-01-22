18ca99e43c7dec1025268023125f05698ec1bfa0
Ivo Tsonev <a1e6e98@rewe-group.com>
2026-01-22 17:35:01 +0200

    feat(remote-config): register repository in DI system
    Add DatabaseRemoteConfigRepository initialization in setupServices()
    to complete the DI registration for the RemoteConfig feature.

--- Sources/App/Entrypoint/Application-Setup.swift
+++ Sources/App/Entrypoint/Application-Setup.swift
@@ -193,6 +193,7 @@ extension Application {
     passwordTokenRepository = DatabasePasswordTokenRepository(database: db)
     generatedRuleRepository = DatabaseGeneratedRuleRepository(database: db)
     waitlistRepository = DatabaseWaitlistRepository(database: db)
+    remoteConfigRepository = DatabaseRemoteConfigRepository(database: db)
 
     // Initialize foundation services (no dependencies)
     randomGeneratorService = RealRandomGeneratorService(app: self)
