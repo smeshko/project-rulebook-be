8ff57c40544216a3b1aae79c88cbef02c863d03a
Ivo Tsonev <a1e6e98@rewe-group.com>
2026-01-26 13:58:24 +0200

    feat(story-RULE-152): create receipts controller
    Add placeholder ReceiptsController struct for future validation
    endpoint handlers to be implemented in Story 2.4.

---
+++ Sources/App/Modules/Receipts/Controllers/ReceiptsController.swift
@@ -0,0 +1,7 @@
+import Fluent
+import Vapor
+
+struct ReceiptsController {
+    // Placeholder controller for receipt validation endpoints
+    // Actual endpoint handlers will be added in Story 2.4
+}
